ca22525fe619648ae3c1d1f67e245c690204e29f
nvjot94 <nvjot94@gmail.com>
2020-03-11 01:59:10 -0400

    implementationp-sass

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react';
-
-import './App.css';
-
+import './App.scss';
+import mainImg from './youtube.png';
 function App() {
   return (
-    <div className="App">
+    <div className="app">
+      <div className='container'> </div>
+      <header><h2> Overview of Sass</h2></header>
+      <main>
+        <div className="main-img">
+          <img
+            src={mainImg}
+            alt="youtube"
+            width="100%"
+            height="100%"
+          />
+          <p>Watch latest video</p>
+        </div>
+        <div className="main-btn">
+          <a href="/#">Subscibe Now!</a>
+        </div>
+      </main>
+
 
     </div>
   );
